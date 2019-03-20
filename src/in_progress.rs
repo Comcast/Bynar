f1cef3a4130709b94765a18b16bb2c2d8254e416
mzhong215 <Michelle_Zhong@comcast.com>
2019-03-20 15:23:04 -0400

    Cut down in_progress

--- src/in_progress.rs
+++ src/in_progress.rs
@@ -620,16 +620,11 @@ pub fn add_disk_detail(
                     disk_info.set_device_database_id(id as u32);
                     Ok(())
                 }
-                Some(i) => {
-                    if i != id as u32 {
-                        Err(BynarError::new(format!(
-                            "Information about {} for storage id {} didn't match",
-                            disk_info.device.name, disk_info.storage_detail_id
-                        )))
-                    } else {
-                        Ok(())
-                    }
-                }
+                Some(id) => Err(BynarError::new(format!(
+                    "Information about {} for storage id {} didn't match",
+                    disk_info.device.name, disk_info.storage_detail_id
+                ))),
+                Some(_) => Ok(()),
             }
         } else {
             // Query said something exists, but we couldn't find that
@@ -797,24 +792,19 @@ pub fn add_or_update_operation_detail(
     }
 
     let stmt_query = conn.query(&stmt, &[])?;
-    match operation_detail.op_detail_id {
-        None => {
-            // insert.
-            if let Some(row) = stmt_query.into_iter().next() {
-                let oid: i32 = row.get("operation_detail_id");
-                operation_detail.set_operation_detail_id(oid as u32);
-                Ok(())
-            } else {
-                Err(BynarError::new(
-                    "Query to insert operation detail into database failed".to_string(),
-                ))
-            }
-        }
-        Some(_) => {
-            // update. even if query to update failed that's fine.
-            Ok(())
+    if operation_detail.op_detail_id.is_none() {
+        // insert.
+        if let Some(row) = stmt_query.into_iter().next() {
+            let oid: i32 = row.get("operation_detail_id");
+            operation_detail.set_operation_detail_id(oid as u32);
+        } else {
+            return Err(BynarError::new(
+                "Query to insert operation detail into database failed".to_string(),
+            ));
         }
     }
+    // update. even if query to update failed that's fine.
+    Ok(())
 }
 
 pub fn save_state(
@@ -1113,9 +1103,5 @@ pub fn is_hardware_waiting_repair(
     }
 
     let stmt_query = conn.query(&stmt, &params)?;
-    if stmt_query.is_empty() {
-        Ok(false)
-    } else {
-        Ok(true)
-    }
+    Ok(!stmt_query.is_empty())
 }
