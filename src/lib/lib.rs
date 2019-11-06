4091a33ae697c7b316783dca890d1ba3739ded0e
mzhong820 <Michelle_Zhong2@comcast.com>
2019-11-06 14:24:49 -0500

    Reverting ZMQ back to ~0.8 because CurveKeyPair in 0.9 up is now in binary format instead of z85-encoded representation

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -32,11 +32,16 @@ where
 pub fn connect(host: &str, port: &str, server_publickey: &str) -> BynarResult<Socket> {
     debug!("Starting zmq sender with version({:?})", zmq::version());
     let context = zmq::Context::new();
+    debug!("Created Context");
     let requester = context.socket(zmq::REQ)?;
+    debug!("Create new REQUESTER");
     let client_keypair = zmq::CurveKeyPair::new()?;
-
-    requester.set_curve_serverkey(server_publickey.as_bytes())?;
+    debug!("Create new keypair");
+    debug!("server public key {:?}", server_publickey);
+    requester.set_curve_serverkey(server_publickey)?;
+    debug!("ServerKey Set");
     requester.set_curve_publickey(&client_keypair.public_key)?;
+    debug!("Public Key Set");
     requester.set_curve_secretkey(&client_keypair.secret_key)?;
     debug!("Connecting to tcp://{}:{}", host, port);
     assert!(requester
@@ -70,7 +75,7 @@ pub fn add_disk_request(
 
     let encoded = o.write_to_bytes().unwrap();
     debug!("Sending message");
-    s.send(encoded, 0)?;
+    s.send(&encoded, 0)?;
 
     debug!("Waiting for response");
     let add_response = s.recv_bytes(0)?;
@@ -125,7 +130,7 @@ pub fn list_disks_request(s: &Socket) -> BynarResult<Vec<Disk>> {
     debug!("{:?}", encoded);
 
     debug!("Sending message");
-    s.send(encoded, 0)?;
+    s.send(&encoded, 0)?;
 
     debug!("Waiting for response");
     let disks_response = s.recv_bytes(0)?;
@@ -147,7 +152,7 @@ pub fn safe_to_remove_request(s: &Socket, path: &Path) -> BynarResult<bool> {
     o.set_disk(format!("{}", path.display()));
     let encoded = o.write_to_bytes()?;
     debug!("Sending message");
-    s.send(encoded, 0)?;
+    s.send(&encoded, 0)?;
 
     debug!("Waiting for response");
     let safe_response = s.recv_bytes(0)?;
@@ -176,7 +181,7 @@ pub fn remove_disk_request(
 
     let encoded = o.write_to_bytes()?;
     debug!("Sending message");
-    s.send(encoded, 0)?;
+    s.send(&encoded, 0)?;
 
     debug!("Waiting for response");
     let remove_response = s.recv_bytes(0)?;
@@ -243,7 +248,7 @@ pub fn get_jira_tickets(s: &Socket) -> BynarResult<()>{
     o.set_Op_type(Op::GetCreatedTickets);
     let encoded = o.write_to_bytes()?;
     debug!("Sending message in get_jira_tickets");
-    s.send(encoded, 0)?;
+    s.send(&encoded, 0)?;
 
     debug!("Waiting for response: get_jira_tickets");
     let tickets_response = s.recv_bytes(0)?;
